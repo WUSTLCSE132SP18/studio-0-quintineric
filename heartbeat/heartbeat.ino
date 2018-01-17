da4cacac2e7dc66b137f917db1f814eebd239d96
QuintinFrerichsWUSTL <quintin.frerichs@gmail.com>
2018-01-17 13:38:21 -0600

    first program done

---
+++ heartbeat/heartbeat.ino
@@ -0,0 +1,12 @@
+/*
+This program prints the number of seconds that have elapsed since the
+program was reset.
+*/
+void setup(){
+    Serial.begin(9600);
+}
+void loop(){
+    delay(1000);
+    Serial.print(millis()/1000);
+    Serial.println(" sec have elapsed");
+}
